c090f5f1664cb563ce52c5a25e844add21c47b5b
Alberto <a-f-r@hotmail.es>
2021-02-17 01:34:06 +0100

    Install request package and add signup form from Bootstrap

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname));
 
 app.get("/", function(req, res){
-  res.send("Server is up and running.")
+  res.sendFile(__dirname + "/signup.html")
 })
 
 
