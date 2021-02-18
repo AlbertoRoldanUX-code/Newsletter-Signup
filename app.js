c387f207f6fa8ee1953aabc8f9e1c54298df48bc
Alberto <a-f-r@hotmail.es>
2021-02-19 00:13:59 +0100

    Change express static function and create public folder

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html")
 })
+
 
 
 app.listen(3000, function(){
